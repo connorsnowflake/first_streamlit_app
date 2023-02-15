c6f4d7d66b9595c7eb9f665af4d3d2561fd8ad6e
connorsnowflake <124177636+connorsnowflake@users.noreply.github.com>
2023-02-15 11:28:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-
 
 # write your own comment -what does the next line do? 
 # i think it normalizes the json file?
