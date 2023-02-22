00b92ba766d8d9dc367ad2467fe377aa26e09156
connorsnowflake <124177636+connorsnowflake@users.noreply.github.com>
2023-02-22 09:16:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,25 +23,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
+#New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-# write your own comment -what does the next line do? i think it normalizes the json file?
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# write your own comment - what does this do? makes it into a dataframe?
-streamlit.dataframe(fruityvice_normalized)
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+ except URLError as e:
+    streamlit.error()
+    
 
 # don't run anything past this line
 streamlit.stop()
-
-
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
