7e04d2f1101d56989b4a7e5b0f1426b8e4cd6086
connorsnowflake <124177636+connorsnowflake@users.noreply.github.com>
2023-02-22 08:50:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,12 @@
-
 import streamlit
 import pandas
-
 streamlit.title('My Parents New Healthy Diner')
-
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -29,18 +23,19 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-# write your own comment -what does the next line do? 
-# i think it normalizes the json file?
-
+# write your own comment -what does the next line do? i think it normalizes the json file?
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-# write your own comment - what does this do?
-# makes it into a dataframe?
-
+# write your own comment - what does this do? makes it into a dataframe?
 streamlit.dataframe(fruityvice_normalized)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+# don't run anything past this line
+streamlit.stop()
+
+
 
 import snowflake.connector
 
